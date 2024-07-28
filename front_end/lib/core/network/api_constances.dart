378219099289c73d7a1009c47f1235b4a983e425
mohamad hadi <87898758+m-hadi404@users.noreply.github.com>
2024-07-28 22:42:40 +0300

    Update api_constances.dart

--- front_end/lib/core/network/api_constances.dart
+++ front_end/lib/core/network/api_constances.dart
@@ -1,5 +1,5 @@
 class ApiConstances {
-  static const String _baseUrl = "https://668f0e40bf9912d4c930786c.mockapi.io";
+  static const String _baseUrl = "https://dummyjson.com";
   static const String _baseImgUrl = 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/';
 
   static const String usersPath = "$_baseUrl/users";
